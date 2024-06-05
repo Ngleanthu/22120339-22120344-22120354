bc416ce3843808d7fe7158552c00f58e40c68269
Ngleanthu <nguyenleanhthu21122003@gmail.com>
2024-06-05 13:39:42 +0700

    update struct

--- src/functions/Misc/Misc.cpp
+++ src/functions/Misc/Misc.cpp
@@ -11,18 +11,3 @@ void clear_screen()
 {
     std::cout << "\033[2J\033[1;1H";
 }
-
-void removeHiddenNewlineChar(std::string &str)
-{
-    // Classic newline
-    if (str[str.length() - 1] == '\n')
-    {
-        str.erase(str.length() - 1);
-    }
-
-    // Windows newline
-    if (str[str.length() - 1] == '\r')
-    {
-        str.erase(str.length() - 1);
-    }
-}
